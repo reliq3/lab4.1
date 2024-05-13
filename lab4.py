05158f1f18090bec01a0ebc5d0f0eb3a705d90a9
reliqus3 <bomfor@mail.ru>
2024-05-13 20:56:56 +0300

    Add file

--- lab4.py
+++ lab4.py
@@ -1,2 +1,16 @@
-x = input()
-print(x)
+import random
+import string
+def generate_passwords(N, K):
+    passwords = []
+    characters = string.ascii_letters + string.digits
+    while len(passwords) < N:
+        password = ''.join(random.choice(characters) for _ in range(K))
+        if password not in passwords:
+            passwords.append(password)
+    return passwords
+N = int(input("Введите количество паролей N: "))
+K = int(input("Введите длину паролей K: "))
+passwords = generate_passwords(N, K)
+print("Сгенерированные пароли:")
+for password in passwords:
+    print(password)
